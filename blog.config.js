5f36ee432171bfd46a4bcc23da4a24478ac7da7f
含着泡面饮开水 <116805224+Tuwilt@users.noreply.github.com>
2024-01-31 23:54:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -49,16 +49,13 @@ const BLOG = {
     FONT_URL: [
         // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
         'https://fonts.googleapis.com/css?family=Roboto&display=swap',
-        /*
         'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
         'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
-        */
     ],
     // 无衬线字体 例如'"LXGW WenKai"'
     FONT_SANS: [
         '"Microsoft YaHei"',
         // '"LXGW WenKai"',
-        /*
         '"PingFang SC"',
         '-apple-system',
         'BlinkMacSystemFont',
@@ -74,7 +71,6 @@ const BLOG = {
         'Arial',
         'sans-serif',
         '"Apple Color Emoji"'
-        */
     ],
     // 衬线字体 例如'"LXGW WenKai"'
     FONT_SERIF: [
